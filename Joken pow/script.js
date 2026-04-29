a37a59fd892e7175c416331b9c622916999a7e89
Gabriel Souza <94180414+Souzagmf@users.noreply.github.com>
2026-04-29 01:55:54 +0100

    Project JOKEN POW

---
+++ Joken pow/script.js
@@ -0,0 +1,10 @@
+const optionImgae = document.querySelectorAll(".option-image")
+
+function HandleOptionClick(event){
+    const clickImage = (event.currentTarget)
+}
+
+optionImgae.forEach(img =>{
+    img.addEventListener("click", HandleOptionClick)
+})
+
