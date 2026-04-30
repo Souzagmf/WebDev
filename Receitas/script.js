8a170d2edbebd52ad67d769e9ff64739ad60d0dd
Souzagmf <94180414+Souzagmf@users.noreply.github.com>
2026-04-30 06:33:00 +0100

    Site de buscar receitas

---
+++ Receitas/script.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('.search-form')
+const recipeList = document.querySelector('.recipe-list')
+const recipedetails = document.querySelector('.recipe-details')
+
+//Take all recipes from server
+async function searchRecipes(ingredient) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+    const data = await response.json()
+
+    showRecipes(data.meals)
+}
+
+function showRecipes(recipes) {
+    recipeList.innerHTML = recipes.map(item => `
+        <div class = 'recipe-card' onclick="getRecipesDetail(${item.idMeal})">
+            <img src= '${item.strMealThumb}' alt='receita-foto'/>
+            <h3>${item.strMeal}</h3>
+        </div>
+        `).join('')
+}
+
+async function getRecipesDetail(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,)
+    const data = await response.json()
+    const recipe = data.meals[0]
+
+    let ingredients = ''
+
+    recipedetails.classList.add('active')
+
+    for (let i = 1; 1 <= 20; i++) {
+        if (recipe[`strIngredient${i}`]) {
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`
+        } else {
+            break
+        }
+    }
+
+    recipedetails.innerHTML = `
+        <div class='line'></div>
+        <h2 class='title'>${recipe.strMeal}</h2>
+        <img src='${recipe.strMealThumb}' alt='${recipe.strMeal}' class='recipe-img'/>
+        <h3>Categoria: ${recipe.strCategory}</h3>
+        <h3>Ingredients:</h3>
+        <ul>${ingredients}</ul>
+        <h3>Instrucões:</h3>
+        <p>${recipe.strInstructions}</p>
+        <p>${recipe.strTags}</p>
+        <p class=video-youtube>Video: <a href='${recipe.strYoutube}' target='_blank'>Assista no Youtube</a></p>
+    `
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const inputValue = event.target[0].value
+    searchRecipes(inputValue)
+})
